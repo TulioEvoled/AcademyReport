251a0c32ffdcbbf79d7b3be91bcae32ac60ef677
TulioEvoled <loltuliox@gmail.com>
2025-03-14 17:24:51 -0600

    AcademyReport version 2.2

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ atexit.register(lambda: scheduler.shutdown())
 
 # **📌 Ejecutar la aplicación**
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8050, debug=False)
